3540657fdf34d490c7bf8cb15579e67c44dbfc0c
tengtengcai <1360246281@qq.com>
2018-04-23 21:41:49 +0800

    delete irrelevant information

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,5 @@
-
-def insertionSort(a):
+#an original edition from introduce to algorithms(v3) pseudocode
+def insertionSort1(a):
     alist = a.copy()
     for j in range(0,len(alist)):
         key = alist[j]
@@ -10,34 +10,17 @@ def insertionSort(a):
             alist[i+1] = key
     return alist
 
-def search(alist,val):
-    pos = []
-    length = len(alist)
-    for i in range(length):
-        if alist[i] == val:
-            pos.append(i)
-    return pos
+#a more modern edition
 
+def insertionSort2(a):
+    alist = a.copy()
+    for j in range(len(alist)):
+        for i in range(j):
+            if  alist[i]>alist[j]:
+                alist[i],alist[j] = alist[j],alist[i]
+    return alist
 
-def midsearch(alist,val):
-    #assume alist is an ordered list(1<...<n)
-    n = len(alist)
-    if n==1:
-        if alist[0] == val:
-            return True
-        else:
-            return False
-    mid = int(n/2)
-    print('mid:',mid)
-    if val>=alist[mid]:
-        print('big')
-        return midsearch(alist[mid:],val)
-    else:
-        print('small')
-        return midsearch(alist[:mid],val)
-
-print(midsearch(insertionSort(a),5))
-if __name__ == '__main__':
+if __name__ == '__main__':    
     a = [5,2,4,6,1,3]
-print(insertionSort(a))
-print(search(a,5))
+    print(insertionSort1(a))
+    print(insertionSort2(a))
